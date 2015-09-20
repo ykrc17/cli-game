4d582bebeb182b1bc1f6f83b1dc32f68142db457
ykrc17 <ykrc17@163.com>
2015-09-21 00:09:09 +0800

    按下backspace可以删去字符

--- js/game.js
+++ js/game.js
@@ -20,12 +20,14 @@ window.onload = function() {
   var MAX_MESSAGE_QUEUE_SIZE = GAME_HEIGHT / LINE_HEIGHT - 1;
 
   var KEY_CODE_ENTER = 13;
+  var KEY_CODE_BACKSPACE = Phaser.Keyboard.BACKSPACE;
 
   var COLOR_WHITE = '#FFFFFF';
   var COLOR_BLACK = '#000000';
   var COLOR_GREEN = '#00FF00';
 
   var messageQueue = [];
+  var cursorPosition = 0;
   var showCursor = false;
 
   function create() {
@@ -36,7 +38,7 @@ window.onload = function() {
 
     var keyboard = game.input.keyboard;
     keyboard.addCallbacks(this, null, keyUp, keyPress);
-    keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+    keyboard.addKey(KEY_CODE_BACKSPACE);
 
     game.time.events.loop(500, function() {
       showCursor = !showCursor;
@@ -58,10 +60,14 @@ window.onload = function() {
     } else {
       text = '  ' + text;
     }
-    if (isLast && showCursor) {
-      text += '_';
-    }
     game.debug.text(text, 2, LINE_HEIGHT * (parseInt(line) + 1), COLOR_GREEN);
+    if (isLast && showCursor) {
+      var cursorText = '';
+      for (var i = 0; i < cursorPosition + 2; i++) {
+        cursorText += ' ';
+      }
+      game.debug.text(cursorText + '_', 2, LINE_HEIGHT * (parseInt(line) + 1), COLOR_GREEN);
+    }
   }
 
   function keyUp(event) {
@@ -69,6 +75,9 @@ window.onload = function() {
     switch (event.keyCode) {
       case KEY_CODE_ENTER:
         handleCommand();
+        break;
+      case KEY_CODE_BACKSPACE:
+        shiftCommand();
         break;
     }
   }
@@ -81,11 +90,15 @@ window.onload = function() {
 
   function handleCommand() {
     var commandLine = getCommandLine().split(' ');
-
     if (commandLine.length < 1) {
       return;
     }
+
     var command = commandLine[0];
+    if (command.length < 1) {
+      newLine();
+      return;
+    }
 
     switch (command) {
       case 'help':
@@ -100,8 +113,18 @@ window.onload = function() {
     }
   }
 
+  // 输入文字
   function appendCommand(char) {
     messageQueue[messageQueue.length - 1].text += char;
+    cursorPosition++;
+  }
+
+  // 删除文字
+  function shiftCommand() {
+    if (messageQueue[messageQueue.length - 1].text.length > 0) {
+      messageQueue[messageQueue.length - 1].text = messageQueue[messageQueue.length - 1].text.substr(0, messageQueue[messageQueue.length - 1].text.length - 1);
+      cursorPosition--;
+    }
   }
 
   function getCommandLine() {
@@ -129,5 +152,6 @@ window.onload = function() {
     while (messageQueue.length > MAX_MESSAGE_QUEUE_SIZE) {
       messageQueue.shift();
     }
+    cursorPosition = 0;
   }
 };
